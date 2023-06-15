064143c628466573aacbc0317cedc242ed8f5b93
Matheus Silva <ms25022003@gmail.com>
2023-06-15 19:34:02 -0300

    feat: create project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import type { AppProps } from "next/app";
+
+import { CssBaseline } from "@mui/material";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  );
+}
